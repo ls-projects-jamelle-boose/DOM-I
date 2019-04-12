0ae8ea6b41b822a3ee8318d5600bebb3605e93ca
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-04-12 08:27:38 -0700

    halfway done

--- js/index.js
+++ js/index.js
@@ -37,6 +37,59 @@ const siteContent = {
   },
 };
 
-// Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
+// Elements
+const logo = document.getElementById("logo-img");
+const navLink = document.querySelectorAll("header nav a")
+const dia = document.querySelector(".cta-text h1")
+const button = document.querySelector(".cta-text button")
+const ctaElementImg = document.querySelector("#cta-img")
+const topH4 = document.querySelectorAll(".main-content .top-content div h4") 
+const topP = document.querySelectorAll(".main-content .top-content .text-content p")
+const middleElementImg = document.getElementById("middle-img")
+
+
+
+
+
+
+
+
+// Add Navigation Link
+navLink.forEach((link, index) => {
+  link.innerHTML = siteContent["nav"][`nav-item-${index + 1}`]
+})
+// Update Img Src
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// Insert "DOM IS AWESOME"
+dia.innerHTML = siteContent["cta"]["h1"]
+
+// Set attributes on Button
+button.innerText = siteContent["cta"]["button"]
+
+// Set CTA IMG SRC
+ctaElementImg.setAttribute("src", siteContent["cta"]["img-src"])
+
+// Add h4 Text in Main Content
+topH4[0].innerText = siteContent["main-content"]["features-h4"]
+topH4[1].innerText = siteContent["main-content"]["product-h4"]
+
+// Add p Text in Main Content
+topP[0].innerText = siteContent["main-content"]["features-content"]
+topP[1].innerText = siteContent["main-content"]["product-content"]
+
+// Add Middle img
+middleElementImg.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+
+/*
+ oh shit! it shares terminals too
+
+yeahh and im trying to install a browser plugin
+that terminal shit is really cool though. i also really like that we don't have to stop and wait for the oteer person to finsish typing
+same
+
+all right dude, im gonna sync to github and head to work, fuck the job gonna set up the environment in the office LOL
+be back online in a couple hours, like 2yep yep
+lmao, aight, see ya soon dude peace
+*/
